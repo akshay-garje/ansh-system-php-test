e347ec0c130a6336adc0343f50d64501e5b32a2d
Akshay <heartyjay175@gmail.com>
2018-11-25 21:07:48 +0530

    Task-3 Completed

--- calculator.php
+++ calculator.php
@@ -15,12 +15,14 @@ function performMathematicalOperations( $arguments )
 {
   	if(isset($arguments) && count($arguments)>1) {
 	  	array_shift($arguments);
-		$operation 		= isset($arguments[0])? strtolower($arguments[0]) : "" ;
+	  	$arr_delimeters = array(",","\n","n");
+		$operation 		= isset($arguments[0]) ? strtolower($arguments[0]) : "" ;
 		$str_values 	= isset($arguments[1]) ? $arguments[1] : "";
 		if($operation=='add') {
 			$answer = 0; 
 			if($str_values!='') {
-				$arr_values = explode(',',$str_values);
+				// function call to get values for operation
+				$arr_values = multipleExplode($arr_delimeters,$str_values);
 				if(isset($arr_values) && count($arr_values)>0) {
 					foreach ($arr_values as $key => $value) {
 						if(is_numeric($value)){
@@ -40,5 +42,21 @@ function performMathematicalOperations( $arguments )
   	}
 }
 
-exit (0)
+/**
+ * { This function is to convert string to array with multiple delimeters }
+ *
+ * @param      array   $delimiters  Delimeters will be an array of multiple delimeters
+ * @param      string  $string      String is an entered/given expression.
+ */
+function multipleExplode($delimiters,$string) {
+    $ready = str_replace($delimiters, $delimiters[0], $string);
+    $ready = str_replace("\\", "", $ready);
+    if($ready!=$delimiters[0]){
+    	$arr_explode = explode($delimiters[0], $ready);
+    	return  $arr_explode;
+    }
+	return  array();
+}
+
+exit (0);
 ?>
